75aa35000e35f6275d15e0a5fae63521cb93d34c
z004xcth <karatas.kaan@siemens.com>
2024-03-27 02:00:46 +0300

    Non-scripts and properties pushed

---
+++ src/test/java/com/siemens/MockICardApp/MockICardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemens.MockICardApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockICardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
