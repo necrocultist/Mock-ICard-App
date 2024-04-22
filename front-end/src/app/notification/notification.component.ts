5c5c12a11d8facb5b43f919cc6fcbb0488dcc859
necrocultist <karataskn20@gmail.com>
2024-04-22 05:09:44 +0300

    Created seperate modal for notifications

---
+++ front-end/src/app/notification/notification.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.css']
+})
+export class NotificationComponent {
+  @Input() message: string = '';
+  @Input() showNotification: boolean = false;
+}
