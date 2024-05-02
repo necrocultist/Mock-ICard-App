d48526e84ba993af34a3322fa8d0cfad56870516
necrocultist <karataskn20@gmail.com>
2024-05-02 22:21:08 +0300

    Updated the query used in event times

--- back-end/src/main/java/com/siemens/MockICardApp/repository/EmployeeEventRepository.java
+++ back-end/src/main/java/com/siemens/MockICardApp/repository/EmployeeEventRepository.java
@@ -2,11 +2,18 @@ package com.siemens.MockICardApp.repository;
 
 import com.siemens.MockICardApp.data.model.entity.EmployeeEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
 public interface EmployeeEventRepository extends JpaRepository<EmployeeEvent, String> {
     List<EmployeeEvent> findByEmployeeId(String employeeId);
+
+    @Query("SELECT e FROM EmployeeEvent e WHERE e.employee.id = :employeeId AND e.eventTime BETWEEN :startTime AND :endTime")
+    List<EmployeeEvent> findByEmployeeIdAndTimeRange(String employeeId, String startTime, String endTime);
+
+    List<EmployeeEvent> findByEmployeeIdAndEventTime(String employeeId, String formattedSelectedDay);
 }
